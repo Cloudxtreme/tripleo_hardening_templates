eb9b2257d093b070262a9e04eca4a267f1051128
Jacob Liberman <jacobliberman@gmail.com>
2016-06-21 17:55:40 -0400

    update deploy to include ceph

--- nova_host.sh
+++ nova_host.sh
@@ -2,3 +2,6 @@
 
 source /home/stack/stackrc
 nova list | awk ' /overcloud/ { print $12" "$4}' | cut -f2 -d= >> /etc/hosts
+awk ' /overcloud/ { print $2 } '  /etc/hosts > /etc/pdsh/machines
+for i in $(nova list | awk ' /overcloud/ { print $4 } ' | cut -f4 -d.); do scp -oStrictHostKeyChecking=no ~/overcloudrc heat-admin@$i:~; done
+pdsh -a -l heat-admin uptime
